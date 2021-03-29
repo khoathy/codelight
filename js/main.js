1ffe8510bf92dad0bd1412cd51d708796e9c4a14
ThyPham <macbook@Macbooks-MBP.lan>
2021-03-29 19:09:18 +0700

    codelight landing page rough

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+
+$(document).ready(function(){
+    var containerHeight = $('.header').height();
+
+    $(window).scroll(function(){
+        //parallax header
+        var wScroll = $(this).scrollTop();
+        // if(wScroll <= containerHeight) {
+        //     $('.header__middle-1').css({
+        //         'transform': 'translate(0%, '+ (wScroll/25) +'%)'
+        //     });
+        //     $('.header__middle-2').css({
+        //         'transform': 'translate(0%, '+ (wScroll/3) +'%)'
+        //     });
+        //     $('.header__back').css({
+        //         'transform': 'translate(0%, '+ wScroll/17 +'%)'
+        //     });
+        //     $('.header__fore').css({
+        //         'transform': 'translate(0%, -'+ wScroll/70 +'%)'
+        //     });
+        // }
+       
+    
+        // sticky header 
+        
+        if(wScroll >= ($('.section-about').offset().top - 650)){
+            $('.nav').addClass('nav__sticky');
+        } else {
+            $('.nav').removeClass('nav__sticky');
+        }
+    });
+
+
+    //menu modal
+    $('.menu').click(function(){
+        $('.nav__menu').toggleClass('visible');
+        console.log('visible toggle')
+    });
+    $(document).click(function(event) {
+        //if click on anything except the modal itself or the "open modal" link, close the modal
+        if (!$(event.target).closest(".nav__menu,.menu").length) {
+          $("body").find(".nav__menu").removeClass("visible");
+        //   console.log('click ouside')
+        }
+    });
+
+});
